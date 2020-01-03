d0ca2868bc2849b70c52fe3cd860ed7be900579d
Christer Eriksson <christer@dogtail.se>
2020-01-03 11:47:59 +0100

    Implement the redirect controller

--- ShortUrl.RedirectApi/Startup.cs
+++ ShortUrl.RedirectApi/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using ShortUrl.RedirectApi.DataAccess;
 
 namespace ShortUrl.RedirectApi
 {
@@ -24,7 +25,8 @@ namespace ShortUrl.RedirectApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllersWithViews();
+            services.AddScoped<IUrlRepository, InMemoryUrlRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

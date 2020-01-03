e0583e66705f0e0b5823f14dabd12fccfc66b21f
Christer Eriksson <christer@dogtail.se>
2020-01-03 11:47:41 +0100

    Add in memory url repository

---
+++ ShortUrl.RedirectApi/DataAccess/IUrlRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShortUrl.RedirectApi.DataAccess
+{
+    public interface IUrlRepository
+    {
+        Task<string> GetUrlAsync(string key);
+    }
+}

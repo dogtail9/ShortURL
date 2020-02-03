3cba5fc82b09ee95e2c2770210cb42a78e0389c7
Christer Eriksson <christer@dogtail.se>
2020-02-03 15:49:55 +0100

    Ta bort ett onödigt NuGet paket

--- ShortUrl.UrlManagementApi/Controllers/ManagementController.cs
+++ ShortUrl.UrlManagementApi/Controllers/ManagementController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
-using System.Web.Http.Description;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;

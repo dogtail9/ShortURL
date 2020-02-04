32054ecee2233953144a73692abb64dda6f5de85
Christer Eriksson <cher12@handelsbanken.se>
2020-02-04 16:27:14 +0100

    Add Migration Host project and build sql script

---
+++ ShortUrl.DataAccess.Sql.MigrationHost/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShortUrl.DataAccess.Sql.MigrationHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

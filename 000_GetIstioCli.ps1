535fa4a18dd2445a77fe5b6a88995748697a600b
Christer Eriksson <christer@dogtail.se>
2020-07-28 10:02:16 +0200

    Funkar!!!

--- 000_GetIstioCli.ps1
+++ 000_GetIstioCli.ps1
@@ -1,3 +1,6 @@
-Invoke-WebRequest -Uri https://github.com/istio/istio/releases/download/1.6.5/istioctl-1.6.5-win.zip -OutFile ./istio.zip
-Expand-Archive -Path ./istio.zip -DestinationPath ./
-Remove-Item ./istio.zip
+if((Test-Path ./istioctl.exe -PathType Leaf) -ne $true)
+{
+  Invoke-WebRequest -Uri https://github.com/istio/istio/releases/download/1.6.5/istioctl-1.6.5-win.zip -OutFile ./istio.zip
+  Expand-Archive -Path ./istio.zip -DestinationPath ./
+  Remove-Item ./istio.zip
+}
--- 030_InstallDashboard.ps1
+++ 030_InstallDashboard.ps1
@@ -1,3 +1,4 @@
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0/aio/deploy/recommended.yaml
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0/aio/deploy/recommended.yaml -OutFile ./Dashboard.yaml
+kubectl apply -f ./Dashboard.yaml
 kubectl describe secret -n kube-system
 kubectl proxy

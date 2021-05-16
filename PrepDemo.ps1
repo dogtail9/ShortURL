8a146b4c621a9a98d9c9a3823fc8ab70d2ec4595
Christer Eriksson <christer@dogtail.se>
2021-05-16 10:30:38 +0200

    .

--- PrepDemo.ps1
+++ PrepDemo.ps1
@@ -1,6 +1,6 @@
 ./000_GetIstioCli.ps1
 ./010_PullDemoImages.ps1
-docker-compose -f .\docker-compose.yml -f .\docker-compose.override.yml -f .\docker-compose.proxy.yml build --parallel
+docker-compose -f .\docker-compose.yml -f .\docker-compose.override.yml build --parallel
 ./020_CreateCacheImage.ps1
 ./030_InstallIstio.ps1
 ./040_InstallDashboard.ps1
